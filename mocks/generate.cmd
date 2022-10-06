47c0e0ba8a4af0f3f1af8cfb8f8b8893948ac03d
Vadim Sabirov <pr0head@gmail.com>
2022-10-06 13:06:35 +0300

    Update latest version

--- mocks/generate.cmd
+++
@@ -1,5 +0,0 @@
-# Use a Vectra Mockery for autogenerate mocks over interfaces
-# https://github.com/vektra/mockery
-
-mockery --name=ConsumerInterface --dir=../ --output=.
-mockery --name=PublisherInterface --dir=../ --output=.
---
+++ mocks/generate.go
@@ -0,0 +1,4 @@
+package mocks
+
+//go:generate mockery --name=ConsumerInterface --dir=../ --output=.
+//go:generate mockery --name=PublisherInterface --dir=../ --output=.
